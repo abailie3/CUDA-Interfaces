8ae81061a708c148c23824675924be458be2f900
abailie3 <albgk@gatech.edu>
2017-01-17 23:12:20 -0600

    Re-arranged functions into header file/kernal file for future integration into tool kit... added transpose, add, and subtract kernals. Still need to add function declarations for the kernals.

--- matOps.cu
+++ matOps.cu
@@ -12,74 +12,41 @@ Adapted from:
 */
 /*
 ============ Change Log ===================
-v0: 1/15/2017
-	-original
+v0: 1/15/2017- original
+v0.01: 1/15/2017- added transpose
 
 ===========================================
 */
 //required headers:
-#include <string.h>
 #include "nodeSet.h" //v0; includes stdio.h
 //optional headers:
 #include <stdlib.h> //I'm here so I don't get fined... aka so malloc doesn't show as error... will compile without
-#include "cuda_runtime.h" //same deal as stdlib.h's comment
-#include "device_launch_parameters.h" //same deal as stdlib.h's comment
 
 
-void print2DMat(Mat2D out, const char* prompt = "") { /*simple method to print matrix
-	needs: nodeSet.h, string.h*/
-	printf("%sMatrix Values:\n{\n", prompt); //just making it pretty
-	for (int i = 0; i < out.rows; ++i) { //iterate through each row/col and print
-		printf("    "); //again, making pretty
-		for (int t = 0; t < out.columns; ++t)
-			printf("%f, ", out.cells[i*out.columns + t]);
-		printf("\n");
-	}
-	printf("}\n");
-	//~~ALB
+__global__ void mAddKernel2D(Mat2D d_a, Mat2D d_b, Mat2D d_o) {
+	int r = blockDim.y * blockIdx.y + threadIdx.y;
+	int c = blockDim.x * blockIdx.x + threadIdx.x;
+	if (r > d_o.rows || c > d_o.columns) return;
+
+	d_o.cells[r * d_o.columns + c] = d_a.cells[r * d_o.columns + c] + d_b.cells[r * d_o.columns + c];
 }
 
-Mat2D vecToMat2D(float f_vector[], int f_rows, int f_cols) { /*convert vector to a mat2D
-	needs: nodeSet.h, print2DMat(Mat2D out)
-	I don't really need stdlib.h, but malloc shows a pesky error if not... will compile without
-	*/
-	//output matrix
-	Mat2D out;
-	out.rows = f_rows;
-	out.columns = f_cols;
+__global__ void mSubKernel2D(Mat2D d_a, Mat2D d_b, Mat2D d_o) {
+	int r = blockDim.y * blockIdx.y + threadIdx.y;
+	int c = blockDim.x * blockIdx.x + threadIdx.x;
+	if (r > d_o.rows || c > d_o.columns) return;
 
-	//allocate memory for matrix
-	out.cells = (float*)malloc(out.rows*out.columns * sizeof(float)); 
-
-	//assign values to matrix
-	for (int i = 0; i < f_rows; ++i)
-		for (int j = 0; j < f_cols; ++j)
-			out.cells[i*f_cols + j] = f_vector[i*f_cols + j];
-	return out;
-	//~~ALB
+	d_o.cells[r * d_o.columns + c] = d_a.cells[r * d_o.columns + c] - d_b.cells[r * d_o.columns + c];
 }
 
-Mat2D cudaMSend2D(Mat2D iM, bool copy, const char* iD) { /*Handles GPU memory allocaion/memory transfer to GPU.
-	needs: nodeset.h
-	copy boolean determines if the matrix values should be copied into the allocated memory on GPU
-	iD takes a constant char pointer of the matrix name/ID
-	*/
-	
-	//device's copy of the input matrix
-	Mat2D d_M; 
-	d_M.rows = iM.rows;
-	d_M.columns = iM.columns;
+__global__ void mTransKernel2D(Mat2D d_i, Mat2D d_o) {
 
-	//allocating memory on GPU for d_M
-	cudaError_t errCode = cudaMalloc(&d_M.cells, d_M.rows * d_M.columns * sizeof(float));
-	printf("Allocating memory for %s on GPU: %s\n", iD, cudaGetErrorString(errCode));
+	int r = blockIdx.y * blockDim.y + threadIdx.y;
+	int c = blockIdx.x * blockDim.x + threadIdx.x;
+	//if out-of-bounds stop
+	if (r > d_o.rows || c > d_o.columns) return;
 
-	//parameter copy decides wheter to copy the iM values to d_M located on GPU
-	if (copy) { 
-		errCode = cudaMemcpy(d_M.cells, iM.cells, d_M.rows * d_M.columns * sizeof(float), cudaMemcpyHostToDevice);
-		printf("Copying %s to GPU: %s\n", iD, cudaGetErrorString(errCode));
-	}
-	return d_M;
+	d_o.cells[r * d_o.columns + c] = d_i.cells[c * d_i.columns + r];
 }
 
 __global__ void mMultKernel2D(Mat2D d_a, Mat2D d_b, Mat2D d_c) { /*CUDA kernal for 2D matrix multiplication
@@ -104,29 +71,28 @@ __global__ void mMultKernel2D(Mat2D d_a, Mat2D d_b, Mat2D d_c) { /*CUDA kernal f
 	//~~ALB
 }
 
-Mat2D cudaMMult2D(Mat2D f_mA, Mat2D f_mB) { /*2D Matrix multiplication algorithm
-	needs: mMultKernel2D<<<nBlocks, tPb>>>(Mat2D d_mA, Mat2D d_mB, Mat2D d_out),
-		   cudaMSend2D(Mat2D f_mX, Bool TF, const char* ID), nodeSet.h
-	*/
+Mat2D cudaMAdd2D(Mat2D f_mA, Mat2D f_mB) {
+	// ADD ERROR HANDLING
 	//Send input matricies to GPU and return d_mA so GPU memory can be deallocated later
+	printf("---------Addition---------\n");
 	Mat2D d_mA = cudaMSend2D(f_mA, true, "matrix A");
 	Mat2D d_mB = cudaMSend2D(f_mB, true, "matrix B");
-	
+
 	//Create output matrix and allocate memory on GPU. Returns d_out to access result/deallocate mem
 	Mat2D out;
 	out.rows = d_mA.rows;
-	out.columns = d_mB.columns;
+	out.columns = d_mA.columns;
 	out.cells = (float*)malloc(out.rows * out.columns * sizeof(float));
 	Mat2D d_out = cudaMSend2D(out, false, "Output matrix");
 
 	//setup CUDA architecture and run kernel
 	dim3 threadsPerBlock(16, 16); //each block will contain 16 by 16 threads
 	dim3 numBlocks((d_out.columns + threadsPerBlock.x - 1) / threadsPerBlock.x, //number of blocks on x dimension of grid
-		            (d_out.rows + threadsPerBlock.y) / threadsPerBlock.y); //number of blocks on y dimension of grid
-	mMultKernel2D <<<numBlocks, threadsPerBlock >>> (d_mA, d_mB, d_out); //run's kernal
+		(d_out.rows + threadsPerBlock.y) / threadsPerBlock.y); //number of blocks on y dimension of grid
+	mAddKernel2D <<<numBlocks, threadsPerBlock >> > (d_mA, d_mB, d_out); //run's kernal
 	cudaError_t errCode = cudaThreadSynchronize(); //synchronize cores to ensure everthing has been run
 	printf("GPU Thread Synchronization: %s\n", cudaGetErrorString(errCode));
-	
+
 	//debug code to find errors in execution of kernel
 	errCode = cudaGetLastError();
 	if (errCode != cudaSuccess)
@@ -138,8 +104,11 @@ Mat2D cudaMMult2D(Mat2D f_mA, Mat2D f_mB) { /*2D Matrix multiplication algorithm
 	//retrieve output matrix from GPU memory
 	errCode = cudaMemcpy(out.cells, d_out.cells, d_out.rows * d_out.columns * sizeof(float), cudaMemcpyDeviceToHost);
 	printf("Pulling Output matrix from GPU: %s\n", cudaGetErrorString(errCode));
-	
-	
+
+	//print result
+	print2DMat(out, "\n\n--Addition Results--\nOutput ");
+	printf("--------------------------------\n");
+
 	//deallocate GPU memory
 	cudaFree(d_mA.cells);
 	cudaFree(d_mB.cells);
@@ -149,7 +118,115 @@ Mat2D cudaMMult2D(Mat2D f_mA, Mat2D f_mB) { /*2D Matrix multiplication algorithm
 	//~~ALB
 }
 
+Mat2D cudaMTrans2D(Mat2D f_m) {
+	printf("--------- Transposition ---------\n");
+	Mat2D d_m = cudaMSend2D(f_m, true, "Original");
 
+	Mat2D out;
+	out.rows = f_m.columns;
+	out.columns = f_m.rows;
+	out.cells = (float*)malloc(out.rows*out.columns * sizeof(float));
+	Mat2D d_o = cudaMSend2D(out, true, "output");
+
+	dim3 tPBlock(16, 16);
+	dim3 nBlocks((out.columns + tPBlock.x - 1) / tPBlock.x, (out.rows + tPBlock.y - 1) / tPBlock.y);
+	mTransKernel2D << <nBlocks, tPBlock >> > (d_m, d_o);
+	cudaError_t errCode = cudaThreadSynchronize(); //synchronize cores to ensure everthing has been run
+	printf("GPU Thread Synchronization: %s\n", cudaGetErrorString(errCode));
+
+	//debug code to find errors in execution of kernel
+	errCode = cudaGetLastError();
+	if (errCode != cudaSuccess)
+	{
+		fprintf(stderr, "ERROR: %s\n", cudaGetErrorString(errCode));
+		exit(-1);
+	}
+
+	//retrieve output matrix from GPU memory
+	errCode = cudaMemcpy(out.cells, d_o.cells, d_o.rows * d_o.columns * sizeof(float), cudaMemcpyDeviceToHost);
+	printf("Pulling Output matrix from GPU: %s\n", cudaGetErrorString(errCode));
+
+	//print result
+	print2DMat(out, "\n\n--Transposition Results--\nOutput ");
+	printf("--------------------------------\n");
+
+	cudaFree(d_m.cells);
+	cudaFree(d_o.cells);
+
+	return out;
+	/*int rows;
+	int cols;
+
+	rows = f_m.rows / par;
+	cols = f_m.columns / par;*/
+
+}
+
+Mat2D cudaMMult2D(Mat2D f_mA, Mat2D f_mB) { /*2D Matrix multiplication algorithm
+											needs: mMultKernel2D<<<nBlocks, tPb>>>(Mat2D d_mA, Mat2D d_mB, Mat2D d_out),
+											cudaMSend2D(Mat2D f_mX, Bool TF, const char* ID), nodeSet.h
+											*/
+
+											//check dimensions, err if incorrect
+	if (f_mA.columns != f_mB.rows) {
+		printf("ERROR: Incorrect array dimensions A*B. Number of columns in A must equal number of rows in B.\n");
+		printf("A: %i x %i, B: %i x %i\n", f_mA.rows, f_mA.columns, f_mB.rows, f_mB.columns);
+		Mat2D err;
+		err.rows = -1;
+		return err;
+	}
+
+	//Send input matricies to GPU and return d_mA so GPU memory can be deallocated later
+	printf("---------Multiplication---------\n");
+	Mat2D d_mA = cudaMSend2D(f_mA, true, "matrix A");
+	Mat2D d_mB = cudaMSend2D(f_mB, true, "matrix B");
+
+	//Create output matrix and allocate memory on GPU. Returns d_out to access result/deallocate mem
+	Mat2D out;
+	out.rows = d_mA.rows;
+	out.columns = d_mB.columns;
+	out.cells = (float*)malloc(out.rows * out.columns * sizeof(float));
+	Mat2D d_out = cudaMSend2D(out, false, "Output matrix");
+
+	//setup CUDA architecture and run kernel
+	dim3 threadsPerBlock(16, 16); //each block will contain 16 by 16 threads
+	dim3 numBlocks((d_out.columns + threadsPerBlock.x - 1) / threadsPerBlock.x, //number of blocks on x dimension of grid
+		(d_out.rows + threadsPerBlock.y) / threadsPerBlock.y); //number of blocks on y dimension of grid
+	mMultKernel2D << <numBlocks, threadsPerBlock >> > (d_mA, d_mB, d_out); //run's kernal
+	cudaError_t errCode = cudaThreadSynchronize(); //synchronize cores to ensure everthing has been run
+	printf("GPU Thread Synchronization: %s\n", cudaGetErrorString(errCode));
+
+	//debug code to find errors in execution of kernel
+	errCode = cudaGetLastError();
+	if (errCode != cudaSuccess)
+	{
+		fprintf(stderr, "ERROR: %s\n", cudaGetErrorString(errCode));
+		exit(-1);
+	}
+
+	//retrieve output matrix from GPU memory
+	errCode = cudaMemcpy(out.cells, d_out.cells, d_out.rows * d_out.columns * sizeof(float), cudaMemcpyDeviceToHost);
+	printf("Pulling Output matrix from GPU: %s\n", cudaGetErrorString(errCode));
+
+	//print result
+	print2DMat(out, "\n\n--Multiplication Results--\nOutput ");
+	printf("--------------------------------\n");
+
+	//deallocate GPU memory
+	cudaFree(d_mA.cells);
+	cudaFree(d_mB.cells);
+	cudaFree(d_out.cells);
+
+	return out;
+	//~~ALB
+}
+
+//Mat2D cudaSSE2d(Mat2D f_y, Mat2D f_h) {
+//	Mat2D d_y = cudaMSend2D(f_y, true);
+//	Mat2D d_f = cudaMSend2D(f_h, true);
+//	Mat2D out;
+//	
+//}
 
 int main(int argc, char* argv) {
 	float a[] = {
@@ -168,29 +245,45 @@ int main(int argc, char* argv) {
 	int rB = 4;
 	int cB = 2;
 
-	//check dimensions, err if incorrect
-	if (cA != rB) {
-		printf("ERROR: Incorrect array dimensions A*B. Number of columns in A must equal number of rows in B.\n");
-		printf("A: %i x %i, B: %i x %i\n", rA, cA, rB, cB);
+	printf("----------- Inputs -----------\n");
+
+	//Check for bad inputs
+	if (sizeof(a) / sizeof(float) != rA*cA) {
+		printf("Number of elements in A does not equal rowA*colA\n");
 		return 0;
-	} 
+	}
+	if (sizeof(b) / sizeof(float) != rB*cB) {
+		printf("Number of elements in B does not equal rowB*colB\n");
+		return 0;
+	}
 
 	//turn input vectors into matricies
 	Mat2D aM = vecToMat2D(a, rA, cA);
-	print2DMat(aM, "A ");
+	print2DMat(aM, "\nA ");
 	Mat2D bM = vecToMat2D(b, rB, cB);
-	print2DMat(bM, "B ");
+	print2DMat(bM, "\nB ");
 
+	
 	//call multiplication algorithm. Returns resulting matrix
 	Mat2D cM = cudaMMult2D(aM, bM);
-
-	//print result
-	print2DMat(cM, "________________________\nResulting ");
+	if (cM.rows == -1) {
+		//dealocate memory from other function calls
+		free(aM.cells);
+		free(bM.cells);
+		return 0;
+	}
+	free(cM.cells);
 	
+	cM = cudaMTrans2D(aM);
+	free(cM.cells);
+	cM = cudaMTrans2D(bM);
+	free(cM.cells);
+	cM = cudaMAdd2D(aM, bM);
 	//dealocate memory from other function calls
 	free(aM.cells);
 	free(bM.cells);
 	free(cM.cells);
+
 
 	return 0;
 	}
